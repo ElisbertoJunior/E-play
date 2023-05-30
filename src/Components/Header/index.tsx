8c17e0992170e67cf00e70ee46ae1855681d196e
Elisberto Junior <elisberto_jr@hotmail.com>
2023-05-30 15:56:20 -0300

    Criando pagina checkout

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,14 +1,25 @@
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { HeaderBar, Links, LinkItem, CartButton } from './styles'
+import {
+  HeaderBar,
+  Links,
+  LinkItem,
+  CartButton,
+  Hamburguer,
+  HeaderRow,
+  NavMobile
+} from './styles'
 import logo from '../../images/logo.svg'
 import cart from '../../images/carrinho.svg'
 import { open } from '../../store/reducers/cart'
 import { RootReducer } from '../../store'
+import { useState } from 'react'
 
 const Header = () => {
   const { items } = useSelector((state: RootReducer) => state.cart)
   const dispatch = useDispatch()
+
+  const [isOpen, setIsOpen] = useState(false)
 
   const openCart = () => {
     dispatch(open())
@@ -16,28 +27,48 @@ const Header = () => {
 
   return (
     <HeaderBar>
-      <div>
-        <Link to={'/'}>
-          <img src={logo} alt="EPLAY" />
-        </Link>
-        <nav>
-          <Links>
-            <LinkItem>
-              <Link to={'/categories'}>Categorias</Link>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Novidades</a>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Promoções</a>
-            </LinkItem>
-          </Links>
-        </nav>
-      </div>
-      <CartButton onClick={openCart}>
-        {items.length} - produto(s)
-        <img src={cart} alt="Carinho" />
-      </CartButton>
+      <HeaderRow>
+        <div>
+          <Hamburguer onClick={() => setIsOpen(!isOpen)}>
+            <span />
+            <span />
+            <span />
+          </Hamburguer>
+          <Link to={'/'}>
+            <img src={logo} alt="EPLAY" />
+          </Link>
+          <nav>
+            <Links>
+              <LinkItem>
+                <Link to={'/categories'}>Categorias</Link>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Novidades</a>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Promoções</a>
+              </LinkItem>
+            </Links>
+          </nav>
+        </div>
+        <CartButton onClick={openCart}>
+          {items.length} <span> - produto(s)</span>
+          <img src={cart} alt="Carinho" />
+        </CartButton>
+      </HeaderRow>
+      <NavMobile className={isOpen ? 'is-open' : ''}>
+        <Links>
+          <LinkItem>
+            <Link to={'/categories'}>Categorias</Link>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </NavMobile>
     </HeaderBar>
   )
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,24 +1,59 @@
 import { styled } from 'styled-components'
 import { colors } from '../../styles'
 
-export const Row = styled.div`
-  margin-top: 16px;
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type RowProps = {
+  marginTop?: string
+}
+
+type ButtonProps = {
+  isActive: boolean
+}
+
+export const Row = styled.div<RowProps>`
+  margin-top: ${(props) => props.marginTop || '0'};
   display: flex;
   gap: 24px;
+  align-items: flex-end;
 `
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
+  max-width: ${(props) => props.maxWidth || 'auto'};
+
   label {
     font-size: 14px;
     margin-bottom: 8px;
     display: block;
   }
 
-  input {
+  input,
+  select {
     background-color: ${colors.whiteColor};
     border: 1px solid ${colors.whiteColor};
     height: 32px;
     padding: 0 8px;
     width: 100%;
+  }
+`
+
+export const TabButton = styled.button<ButtonProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${colors.whiteColor};
+  background-color: ${(props) =>
+    props.isActive ? colors.GreenColor : colors.BlackColor};
+  height: 32px;
+  border: none;
+  margin-right: 16px;
+  padding: 0 8px;
+  margin-bottom: 24px;
+  cursor: pointer;
+
+  img {
+    margin-right: 8px;
   }
 `
